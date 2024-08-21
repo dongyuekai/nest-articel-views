947f2921129e56f1965338c32377067e0f587bd8
dongyuekai <457545648@qq.com>
2024-08-21 15:24:23 +0800

    feat: 实现和mysql建立连接，建库建表，插入数据

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ArticleModule } from './article/article.module';
+import { Article } from './article/entities/article.entity';
+import { User } from './user/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'dong',
+      database: 'article_views',
+      synchronize: true,
+      logging: true,
+      entities: [Article, User],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+    UserModule,
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
