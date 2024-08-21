e9bc488b8f6a8046698dbeedd5d0b2874b574cf4
dongyuekai <457545648@qq.com>
2024-08-21 15:59:44 +0800

    feat: 实现登录接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Session,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -30,5 +40,15 @@ export class UserController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto, @Session() session) {
+    const user = await this.userService.login(loginUserDto);
+    session.user = {
+      id: user.id,
+      username: user.username,
+    };
+    return 'success';
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+import { LoginUserDto } from './dto/login-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
@@ -22,5 +26,23 @@ export class UserService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  @InjectEntityManager()
+  private entityManager: EntityManager;
+
+  async login(loginUser: LoginUserDto) {
+    const user = await this.entityManager.findOne(User, {
+      where: {
+        username: loginUser.username,
+      },
+    });
+    if (!user) {
+      throw new BadRequestException('用户不存在---');
+    }
+    if (user.password !== loginUser.password) {
+      throw new BadRequestException('密码错误---');
+    }
+    return user;
   }
 }
